0597a550f2875b898d96af2ea4d714e99bcac0f5
janoweb <janoweb@users.noreply.github.com>
2021-01-02 19:07:14 +0000

    finish section7

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,105 @@
 <template>
-    <section>
-        <h2>My Friends</h2>
-        <ul>
-            <friend-contact></friend-contact>
-        </ul>
-    </section>
+  <section>
+    <header>
+      <h1>My Friends</h1>
+    </header>
+    <ul>
+      <friend-contact></friend-contact>
+    </ul>
+  </section>
 </template>
 
 <script>
-    export default {
-        data(){
-            return {
-                friends: [
-                    {
-                        id: 1,
-                        name: 'Pablo Quevedo',
-                        phone: '645 123685',
-                        email: 'pablo@quevedo.com'
-                    },
-                    {
-                        id: 2,
-                        name: 'Laura Flor',
-                        phone: '645 854369',
-                        email: 'laura@flowers.com'
-                    },
-                    {
-                        id: 3,
-                        name: 'Señor Q',
-                        phone: '645 854369',
-                        email: 'señor@q.com'
-                    }
-                ]
-            }
+export default {
+  data() {
+    return {
+      friends: [
+        {
+          id: 1,
+          name: "Pablo Quevedo",
+          phone: "645 123685",
+          email: "pablo@quevedo.com",
         },
+        {
+          id: 2,
+          name: "Laura Flor",
+          phone: "645 854369",
+          email: "laura@flowers.com",
+        },
+        {
+          id: 3,
+          name: "Señor Q",
+          phone: "645 854369",
+          email: "señor@q.com",
+        },
+      ],
     };
+  }
+};
 </script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: "Jost", sans-serif;
+}
+
+body {
+  margin: 0;
+}
+
+header {
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  margin: 3rem auto;
+  border-radius: 10px;
+  padding: 1rem;
+  background-color: #58004d;
+  color: white;
+  text-align: center;
+  width: 90%;
+  max-width: 40rem;
+}
+
+#app ul {
+  margin: 0;
+  padding: 0;
+  list-style: none;
+}
+
+#app li {
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  margin: 1rem auto;
+  border-radius: 10px;
+  padding: 1rem;
+  text-align: center;
+  width: 90%;
+  max-width: 40rem;
+}
+
+#app h2 {
+  font-size: 2rem;
+  border-bottom: 4px solid #ccc;
+  color: #58004d;
+  margin: 0 0 1rem 0;
+}
+
+#app button {
+  font: inherit;
+  cursor: pointer;
+  border: 1px solid #ff0077;
+  background-color: #ff0077;
+  color: white;
+  padding: 0.05rem 1rem;
+  box-shadow: 1px 1px 2px rgba(0, 0, 0, 0.26);
+}
+
+#app button:hover,
+#app button:active {
+  background-color: #ec3169;
+  border-color: #ec3169;
+  box-shadow: 1px 1px 4px rgba(0, 0, 0, 0.26);
+}
+</style>
