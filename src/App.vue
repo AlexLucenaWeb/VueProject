7bb6ec915822553e996faf8a4dabed98d1ea0737
janoweb <janoweb@users.noreply.github.com>
2021-01-02 17:55:11 +0000

    starting app

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+    <section>
+        <h2>My Friends</h2>
+        <ul>
+            <li>
+                
+            </li>
+        </ul>
+    </section>
+</template>
+
+<script>
+    export default {
+        data(){
+            return {
+                friends: [
+                    {
+                        id: 1,
+                        name: 'Pablo Quevedo',
+                        phone: '645 123685',
+                        email: 'pablo@quevedo.com'
+                    },
+                    {
+                        id: 2,
+                        name: 'Laura Flor',
+                        phone: '645 854369',
+                        email: 'laura@flowers.com'
+                    },
+                    {
+                        id: 3,
+                        name: 'Señor Q',
+                        phone: '645 854369',
+                        email: 'señor@q.com'
+                    }
+                ]
+            }
+        },
+    };
+</script>
